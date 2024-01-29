1db39de6274b4f6c365248f510d043236fd2e73d
Subhradeep <subhradeeppal30@gmail.com>
2024-01-30 00:45:02 +0530

    update

--- download.py
+++ download.py
@@ -8,23 +8,13 @@ from contextlib import contextmanager
 from pathlib import Path
 import pathlib
 
-@contextmanager
-def set_posix_windows():
-    posix_backup = pathlib.PosixPath
-    try:
-        pathlib.PosixPath = Path.home()
-        yield
-    finally:
-        pathlib.PosixPath = posix_backup
+potato_cat = ('Early_Blight', 'Healthy', 'Late_Blight')
+potato = from_pretrained_fastai("Luna-Skywalker/potato_dtect")
+_save_pretrained_fastai(potato, "ml_models\potato_dtect")
 
-with set_posix_windows():
-    potato_cat = ('Early_Blight', 'Healthy', 'Late_Blight')
-    potato = from_pretrained_fastai("Luna-Skywalker/potato_dtect")
-    _save_pretrained_fastai(potato, "ml_models\potato_dtect")
-
-    corn = from_pretrained_fastai("Luna-Skywalker/corn_dtect")
-    wheat = from_pretrained_fastai("Luna-Skywalker/wheat_dtect")
-    rice = from_pretrained_fastai("Luna-Skywalker/rice_dtect")
-    _save_pretrained_fastai(corn, "ml_models\corn_dtect")
-    _save_pretrained_fastai(wheat, "ml_models\wheat_dtect")
-    _save_pretrained_fastai(rice, "ml_models\Rice_dtect")
+corn = from_pretrained_fastai("Luna-Skywalker/corn_dtect")
+wheat = from_pretrained_fastai("Luna-Skywalker/wheat_dtect")
+rice = from_pretrained_fastai("Luna-Skywalker/rice_dtect")
+_save_pretrained_fastai(corn, "ml_models\corn_dtect")
+_save_pretrained_fastai(wheat, "ml_models\wheat_dtect")
+_save_pretrained_fastai(rice, "ml_models\Rice_dtect")
